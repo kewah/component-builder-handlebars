070db71bbf66ddb80ffe81d72ce9c9f336abb732
Antoine Lehurt <keuwah@gmail.com>
2013-02-27 14:11:38 +0100

    Update tests

--- test/plugin_test.js
+++ test/plugin_test.js
@@ -4,6 +4,10 @@ var vm = require('vm');
 
 describe('Handlerbars plugin', function() {
 
+  var req = function(res, moduleName) {
+    return 'var window = "";' + res.require + res.js + '; require("' + moduleName + '")';
+  };
+
   it('should precompile templates and partials', function(done) {
     var builder = new Builder('test/fixtures/tpl-and-partial');
     builder.use(plugin());
@@ -11,13 +15,13 @@ describe('Handlerbars plugin', function() {
     builder.build(function(err, res) {
       if (err) return done(err);
 
-      var test1 = vm.runInNewContext(res.require + res.js + '; require("test-tpl-and-template/test1")');
+      var test1 = vm.runInNewContext(req(res, 'test-tpl-and-template/test1'));
       test1({}).should.equal('<h1>Hello</h1>');
 
-      var test2 = vm.runInNewContext(res.require + res.js + '; require("test-tpl-and-template/test2")');
+      var test2 = vm.runInNewContext(req(res, 'test-tpl-and-template/test2'));
       test2({ content: 'hi' }).should.equal('<h2>Say hi</h2>');
 
-      var test3 = vm.runInNewContext(res.require + res.js + '; require("test-tpl-and-template/test3")');
+      var test3 = vm.runInNewContext(req(res, 'test-tpl-and-template/test3'));
       test3({ content: 'World' }).should.equal('<p>Partial</p>\n<p>Hello</p>\n<p>World</p>');
 
       return done();
@@ -32,10 +36,10 @@ describe('Handlerbars plugin', function() {
     builder.build(function(err, res) {
       if (err) return done(err);
 
-      var test1 = vm.runInNewContext(res.require + res.js + '; require("test-with-dependencies/test")');
+      var test1 = vm.runInNewContext(req(res, 'test-with-dependencies/test'));
       test1({ content: 'World' }).should.equal('<p>Hello</p>\n<p>World</p>');
 
-      var depTest = vm.runInNewContext(res.require + res.js + '; require("local-component-test/template")');
+      var depTest = vm.runInNewContext(req(res, 'local-component-test/template'));
       depTest({}).should.equal('<div>Local component template</div>\n<div>hey</div>');
 
       return done();
@@ -49,7 +53,7 @@ describe('Handlerbars plugin', function() {
     builder.build(function(err, res) {
       if (err) return done(err);
 
-      var top = vm.runInNewContext(res.require + res.js + '; require("test-nested-partial/views/top")');
+      var top = vm.runInNewContext(req(res, 'test-nested-partial/views/top'));
       top({}).should.equal('<h1>Hello</h1>\n<nav></nav>\n<ul><li></li></ul>');
 
       return done();
@@ -65,7 +69,7 @@ describe('Handlerbars plugin', function() {
     builder.build(function(err, res) {
       if (err) return done(err);
 
-      var tpl = vm.runInNewContext(res.require + res.js + '; require("local-component-test/template")');
+      var tpl = vm.runInNewContext(req(res, 'local-component-test/template'));
       tpl({}).should.equal('<div>Local component template</div>');
 
       return done();
